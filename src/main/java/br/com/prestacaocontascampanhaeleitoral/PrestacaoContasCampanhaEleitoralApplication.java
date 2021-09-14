bc80d05758344504a808017b9e0cfb8a744d83ca
diogo07 <diogosousa36@gmail.com>
2021-09-14 20:56:45 -0300

    init

---
+++ src/main/java/br/com/prestacaocontascampanhaeleitoral/PrestacaoContasCampanhaEleitoralApplication.java
@@ -0,0 +1,13 @@
+package br.com.prestacaocontascampanhaeleitoral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestacaoContasCampanhaEleitoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestacaoContasCampanhaEleitoralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prestacaocontascampanhaeleitoral/PrestacaoContasCampanhaEleitoralApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prestacaocontascampanhaeleitoral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestacaoContasCampanhaEleitoralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
